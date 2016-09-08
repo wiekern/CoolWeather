d05a40baf08e2737983fcc47dcd156fa9c0ea8a3
wiekern <halloweltyan@gmail.com>
2016-09-08 10:31:24 +0200

    Add database class and entity class of every table

---
+++ app/src/main/java/com/wiekern/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.wiekern.coolweather.model;
+
+/**
+ * Created by yafei on 9/8/16.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
